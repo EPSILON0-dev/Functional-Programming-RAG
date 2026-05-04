7dc5dd59ce10f61ef6b8b67ecede4365fe10acb7
EPSILON0-dev <lukaforenc@gmail.com>
2026-05-04 15:42:16 +0200

    feat: add startup script for UI development environment

---
+++ start.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Simple startup script. USED FOR DEVELOPMENT PURPOSES ONLY. DO NOT USE IN PRODUCTION ENVIRONMENT.
+ui_pid=0
+
+print_log()
+{
+    text=$1
+    printf "\033[1m[start.sh]\033[0m %s\n" "$text"
+}
+
+start_ui()
+{
+    print_log "Starting UI..."
+    cd ui && npm run dev &
+    ui_pid=$!
+    print_log "UI started with PID $ui_pid, waiting for it to be ready..."
+    while ! nc -z localhost 5173; do
+        sleep 0.1
+    done
+
+    print_log "UI is ready."
+}
+
+stop_all()
+{
+    print_log "Stopping all processes..."
+    if [ $ui_pid -ne 0 ]; then
+        kill $ui_pid
+        print_log "UI process with PID $ui_pid stopped."
+    fi
+}
+
+main()
+{
+    trap stop_all EXIT
+    start_ui
+}
+
+main
+waitpid $ui_pid
